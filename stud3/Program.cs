a331ee5335ea6c076e7493da10feec0159803026
MiaksaY <kasimovvladlen2006rez@gmail.com>
2023-03-27 22:45:18 +0500

    сохранил доп файлы для работы кода

---
+++ stud3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stud3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum;
+
+            Console.WriteLine("Ведите любое число для проверки на четность");
+            number = int.Parse(Console.ReadLine());
+
+            sum = number % 2;
+
+            if (sum == 0)
+            {
+                Console.WriteLine("Ваше число четное");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Ваше число нечетное");
+                Console.ReadLine();
+            }
+        }
+    }
+}
