b1345fc9d6c755cf31a390e355c7b1b3785f0492
MiaksaY <kasimovvladlen2006rez@gmail.com>
2023-03-27 15:17:34 +0500

    добавил программу с конструкцией if else в stud2

--- stud2/Program.cs
+++ stud2/Program.cs
@@ -10,9 +10,32 @@ namespace stud2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Слава Советскому Союзу");
-            Console.ReadLine();
+            int a;
 
+            Console.WriteLine("Мини-игра 'Угадай число от 170 до 180'");
+            Console.WriteLine("Введите любое число");
+
+            a = int.Parse(Console.ReadLine());
+            if (a == 174)
+            {
+                Console.WriteLine("Вы угадали! Поздравляю!");
+                Console.ReadLine();
+            }
+            else
+            {
+                if (a > 174)
+                {
+                    Console.WriteLine("Загаданное число меньше вашего");
+                    Console.WriteLine("Перезапустите программу и попробуйте еще раз");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Загаданное число больше вашего");
+                    Console.WriteLine("Перезапустите программу и попробуйте еще раз");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
