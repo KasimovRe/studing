339e3cf2fc47b2bfe17ceeda7a230f3eb4798094
MiaksaY <kasimovvladlen2006rez@gmail.com>
2023-03-28 22:51:01 +0500

    добавил еще один кальк - более практичный без лишнего. также добавил циклы и чуть чуть пофиксил ненужное. также отсортировал проекты по папкам

---
+++ calc/calc.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    internal class calc
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2 ;
+            string metod;
+
+            while (true)
+            {   Console.Clear();
+                Console.WriteLine("Введите число 1");
+                n1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите число 2");
+                n2 = double.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Выберите метод операции:");
+                Console.WriteLine("(+) =1, (-) =2, (*) =3, (/) =4");
+                metod = (Console.ReadLine());
+
+                switch (metod)
+                {
+                    case "1":
+                        Console.WriteLine(n1 + n2);
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        Console.WriteLine(n1 - n2);
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                        Console.WriteLine(n1 * n2);
+                        Console.ReadLine();
+                        break;
+                    case "":
+                        if (n2 == 0)
+                        {
+                            Console.WriteLine("0");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine(n1 / n2);
+                            Console.ReadLine();
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Некоректно введены значения");
+                        Console.ReadLine();
+                        break;
+                        // меньше моих предыдущих кодов на >20 строчек. фунционал тот же, только не так "красиво"
+                }
+            }
+        }
+    }
+}
---
+++ calc1/calc1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace studing
+{
+    internal class Program2
+    {
+        static void Main (string[] args)
+        {
+             // пишем простейший калькулятор c switch
+
+            double number1, number2;
+            string simvol;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Для корректной работы простейшего калькулятора, вводите только два числа");
+                Console.WriteLine("При указании алгебраического метода вводите только номер операции");
+                Console.WriteLine("Если программа крашнется, то не обижайтесь)");
+
+                Console.WriteLine("");
+
+                Console.WriteLine("Введите номер операции:");
+                Console.WriteLine("---");
+
+                Console.WriteLine("Сложение (+) = 1");
+                Console.WriteLine("Вычитание (-) = 2");
+                Console.WriteLine("Деление (/) = 3");
+                Console.WriteLine("Умножение (*) = 4");
+
+                simvol = Console.ReadLine();
+
+                if ((simvol == "1") || (simvol == "2") || (simvol == "3") || (simvol == "4"))
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("---");
+                    Console.WriteLine("Введите первое число:");
+                    number1 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число:");
+                    number2 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("");
+
+                    switch (simvol)
+                    {
+                        case "1":
+                            Console.WriteLine("---");
+                            Console.WriteLine("Cумма ваших чисел = " + (number1 + number2));
+                            break;
+                        case "2":
+                            Console.WriteLine("---");
+                            Console.WriteLine("Разность ваших чисел = " + (number1 - number2));
+                            break;
+                        case "3":
+                            if (number2 == 0)
+                            {
+                                Console.WriteLine("---");
+                                Console.WriteLine("Деление на ноль невозможно!");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("---");
+                                Console.WriteLine("Результат деления ваших чисел = " + (number1 / number2));
+                                Console.ReadLine();
+                            }
+                            break;
+                        case "4":
+                            Console.WriteLine("---");
+                            Console.WriteLine("Произведение ваших чисел = " + (number1 * number2));
+                            break;
+                    }
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели некоректное значение");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
