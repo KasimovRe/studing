f4e56a48de62efda79be5a55d53ec0cd557f3973
MiaksaY <kasimovvladlen2006rez@gmail.com>
2023-03-29 19:20:56 +0500

    сделал код на проверку четности чисел и вывод общ кол ва. добавил папку тест

--- schoolWORK/Program.cs
+++ schoolWORK/Program.cs
@@ -10,82 +10,45 @@ namespace schoolWORK
     {
         static void Main(string[] args)
         {
-            // пишем простейший калькулятор if else
+            //написать программу которая считает кол во четных и нечетных чисел в том диапозоне, который указал пользователь
 
-            double number1, number2;
-            string simvol;
-            while (true)
+            uint kolvo, chet, nechet; // uint потому что отриц чисел быть не может 
+            kolvo = 0;
+            chet = 0;
+            nechet = 0;
+
+            Console.WriteLine("Введите диапозон:");
+            Console.WriteLine("От какого числа начать отсчет...");
+            int diapozon1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("До какого числа начать отсчет...");
+            int diapozon2 = int.Parse(Console.ReadLine());
+
+            if (diapozon2 < diapozon1)
             {
-                Console.Clear();
-                Console.WriteLine("Для корректной работы простейшего калькулятора, вводите только два числа");
-                Console.WriteLine("При указании алгебраического метода вводите только номер операции");
-                Console.WriteLine("Если программа крашнется, то не обижайтесь)");
+                Console.WriteLine("Вы ввели неверные значения");
+                Console.ReadLine();
+                return;
+            }
 
-                Console.WriteLine("");
+            int diapozon = diapozon2 - diapozon1;
 
-                Console.WriteLine("Введите номер операции:");
-                Console.WriteLine("---");
 
-                Console.WriteLine("Сложение (+) = 1");
-                Console.WriteLine("Вычитание (-) = 2");
-                Console.WriteLine("Деление (/) = 3");
-                Console.WriteLine("Умножение (*) = 4");
+            while (kolvo < diapozon)
+            {
+                kolvo++;
 
-                simvol = Console.ReadLine();
-
-                if ((simvol == "1") || (simvol == "2") || (simvol == "3") || (simvol == "4"))
-                {
-                    Console.WriteLine("");
-                    Console.WriteLine("---");
-                    Console.WriteLine("Введите первое число:");
-                    number1 = double.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Введите второе число:");
-                    number2 = double.Parse(Console.ReadLine());
-
-                    Console.WriteLine("");
-
-                    if (simvol == "1")
-                    {
-                        Console.WriteLine("---");
-                        Console.WriteLine("Ccумма ваших чисел = " + (number1 + number2));
-                        Console.ReadLine();
-                    }
-                    else if (simvol == "2")
-                    {
-                        Console.WriteLine("---");
-                        Console.WriteLine("Разность ваших чисел = " + (number1 - number2));
-                        Console.ReadLine();
-                    }
-                    else if (simvol == "3")
-                    {
-                        if (number2 == 0)
-                        {
-                            Console.WriteLine("---");
-                            Console.WriteLine("Деление на ноль невозможно!");
-                            Console.ReadLine();
-                        }
-                        else
-                        {
-                            Console.WriteLine("---");
-                            Console.WriteLine("Результат деления ваших чисел = " + (number1 / number2));
-                            Console.ReadLine();
-                        }
-                    }
-                    else if (simvol == "4")
-                    {
-                        Console.WriteLine("---");
-                        Console.WriteLine("Произведение ваших чисел = " + (number1 * number2));
-                        Console.ReadLine();
-                    }
-                }
+                if (kolvo % 2 == 0)
+                    chet++;
                 else
-                {
-                    Console.WriteLine("");
-                    Console.WriteLine("Вы ввели некоректное значение");
-                    Console.ReadLine();
-                }
-            }        
+                    nechet++;
+            }
+
+            Console.WriteLine("Кол во четных чисел = " + chet);
+            Console.WriteLine("Кол во нечетных чисел = " + nechet);
+            Console.WriteLine("Сумма всех чисел/ диапозон = " + (chet + nechet));
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kolvo = 0;
+
+            var i = 1;
+
+            for (Console.WriteLine("Начало выполнения цикла"); i < 4; Console.WriteLine($"i = {i}"))
+            {
+                i++;
+            }
+        }
+    }
+}
