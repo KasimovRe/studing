7c7f299500689d646072a0e5d3424a9a11898346
kasimovvladlen2006rez@gmail.com <kasimovvladlen2006rez@gmail.com>
2023-04-22 23:35:58 +0500

    начал изучать объекты

---
+++ classes and objects/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Company apple = new Company();
+
+            string applePhone = apple.Phone;
+            int appleAge = apple.Age;
+
+            apple.Age = 2001;
+            apple.Phone = "Iphone";
+
+            apple.Print();
+
+        }
+    }
+
+    class Company
+    {
+        public string Phone = "None";
+        public int Age;
+
+        public void Print()
+        {
+            Console.WriteLine($"Марка телефона: {Phone}\t| Год сборки: {Age}");
+        }
+    }
+}
