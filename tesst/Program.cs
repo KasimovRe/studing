d70339a66aac8a08c5d93d790fc5c7135aff3ced
kasimovvladlen2006rez@gmail.com <kasimovvladlen2006rez@gmail.com>
2023-05-13 22:11:31 +0500

    голова не варит... прошел is as и перобразование типов. задачи на завтра

--- tesst/Program.cs
+++ tesst/Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp7
+namespace HelloApp
 {
+    // Что будет выведено на консоль в результате выполнения следующей программы и почему?:
     class Program
     {
-
+        static void Main(string[] args)
+        {
+            Person tom = new Person { Name = "Tom" };
+            Employee empl = tom as Employee;
+            tom.Name = "Bob";
+            Console.WriteLine(empl.Name);
+            Console.ReadKey();
+        }
     }
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+    class Employee : Person { }
 }
