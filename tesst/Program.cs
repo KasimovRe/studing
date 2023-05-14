755c08da5e120fd9b99e7f58a72f37ee94ae4df5
kasimovvladlen2006rez@gmail.com <kasimovvladlen2006rez@gmail.com>
2023-05-14 23:53:48 +0500

    + повторил приведение типов. (доизучать)

--- tesst/Program.cs
+++ tesst/Program.cs
@@ -7,22 +7,64 @@ using System.Threading.Tasks;
 
 namespace HelloApp
 {
-    // Что будет выведено на консоль в результате выполнения следующей программы и почему?:
+    // Создайте класс Employee с именем и зарплатой.
+    // Создайте класс Contractor наследующийся от Employee.
+    // Добавьте метод для преобразования Employee в Contractor.
     class Program
     {
         static void Main(string[] args)
         {
-            Person tom = new Person { Name = "Tom" };
-            Employee empl = tom as Employee;
-            tom.Name = "Bob";
-            Console.WriteLine(empl.Name);
-            Console.ReadKey();
+            Employee employee = new Employee("Бодя", 23500);
+
+            Contractor contractor = new Contractor("opdfgfip", 34000);
+            employee.ConvertToContractor(contractor);
+
+            contractor.Print(employee);
         }
     }
 
-    class Person
+    class Employee
     {
+        public Employee(string name, int salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+
         public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public Contractor ConvertToContractor(Contractor contractor)
+        {
+            if (contractor is Employee)
+            {
+                Employee employee = contractor;
+                return (Contractor)employee;
+            }
+            return null;
+
+        }
     }
-    class Employee : Person { }
+
+
+    class Contractor : Employee
+    {
+        public Contractor(string name, int salary) : base(name, salary) { }
+
+        public Employee ConvertToEmpoyee(Employee employee)
+        {
+            if (employee is Contractor contractor)
+            {
+                return contractor;
+            }
+            return null;
+        }
+        public void Print(Employee employee)
+        {
+            Console.WriteLine(employee.Name);
+            Console.WriteLine(employee.Salary);
+        }
+    }
 }
+
