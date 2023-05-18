7d40c8a9d1b3a896fc4c8470d750885d246ce5d5
kasimovvladlen2006rez@gmail.com <kasimovvladlen2006rez@gmail.com>
2023-05-18 19:46:43 +0500

    + абстрактный класс

---
+++ abstract class/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transport car = new Car("машина");
+            Transport ship = new Ship("корабль");
+            Transport aircraft = new Aircraft("самолет");
+
+            car.Move();         // машина движется
+            ship.Move();        // корабль движется
+            aircraft.Move();    // самолет движется
+        }
+    abstract class Transport
+        {
+            public string Name { get; }
+            // конструктор абстрактного класса Transport
+            public Transport(string name)
+            {
+                Name = name;
+            }
+            public void Move() => Console.WriteLine($"{Name} движется");
+
+        }
+        // класс корабля
+        class Ship : Transport
+        {
+            // вызываем конструктор базового класса
+            public Ship(string name) : base(name) { }
+        }
+        // класс самолета
+        class Aircraft : Transport
+        {
+            public Aircraft(string name) : base(name) { }
+        }
+        // класс машины
+        class Car : Transport
+        {
+            public Car(string name) : base(name) { }
+        }
+    }
+}
---
+++ avbstractGPT/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avbstractGPT
+{
+    // Создать абстрактный класс Figure с абстрактным методом calculateArea().
+    // Унаследовать от него классы Circle и Rectangle и реализовать в них
+    // метод calculateArea() для вычисления площади круга и прямоугольника.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure circle = new Circle()
+            {
+                R = 10
+            };
+
+            Figure rectangle = new Rectangle()
+            {
+                Weight = 5,
+                Height = 3
+            };
+
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(rectangle.CalculateArea());
+
+        }
+    }
+    abstract class Figure { public abstract double CalculateArea(); }
+
+    class Circle : Figure
+    {
+        public int R { get; set; }
+
+        public override double CalculateArea()
+        {
+            double S = (Math.Pow(R, 2) * 3.14);
+            return S;
+        }
+    }
+
+    class Rectangle : Figure
+    {
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public override double CalculateArea()
+        {
+            double S = Weight * Height;
+            return S;
+        }
+    }
+}
