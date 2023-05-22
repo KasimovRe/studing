918fe16acc1a77624a7c4a7e214d5521a3e9339a
kasimovvladlen2006rez@gmail.com <kasimovvladlen2006rez@gmail.com>
2023-05-22 23:12:08 +0500

    подучил интерфейсы. завтра сделаю конечное дз

--- interfaceGPT2/Program.cs
+++ interfaceGPT2/Program.cs
@@ -16,23 +16,62 @@ namespace interfdaceGPT2
         // Sword(меч) - реализует IWeapon
 
         // То есть, рыцарь является персонажем и имеет оружие - меч.
+
         static void Main(string[] args)
         {
+            Sword sword = new Sword();
+            Warrior warrior = new Warrior();
+            warrior.Weapon = sword;
+
+            warrior.Attack(); // Со свистом разя мечом!
+
+            Gun gun = new Gun();
+            warrior.Weapon = gun;
+            warrior.Attack();
+
+            Console.ReadKey();
         }
     }
     interface IWeapon
     {
-        void Attack();
+        void AttackWeapon(); // метод атака. его должен реализовывать каждый объект оружия
     }
     interface ICharacter
     {
-        IWeapon Weapon { get; set; } // объявляем свойство интерфейса IWeapon
+        void Attack(); // отдельный метод атаки уже для человека
+        IWeapon Weapon { get; set; } // используя тип интерфейса оружия, мы передаем в него значение
+                                     // какой меч выбираем. то есть для этого изначально нужно объявить 
+                                     // объект класс Sword от IWeapon
     }
 
-    class Warrior : ICharacter
+    /////////////////////////
+    
+    class Warrior : ICharacter // реализуем интерфейс ЧАРАКТЕР. там 1 метод и 1 свойство типа интерфейса ВИАПОН
     {
-        public IWeapon Weapon { get; set; }
+        public IWeapon Weapon { get; set; } // обязательно передаем значение меча, от интерфейса
+                                            // ВИАПОН и соответствующего класса
 
+        public void Attack()
+        {
+            Weapon.AttackWeapon(); // так как объект ВИАПОН унаследован от интерфейса, то при вызове
+                                   // метода АТАК черещ оружие, он вызывет его от класса объекта от
+                                   // интерфейса
+        }
+    }
 
+    class Sword : IWeapon // по лайту. просто создаем меч от иетерфейса и выдаем ему метод атаки
+    {
+        public void AttackWeapon()
+        {
+            Console.WriteLine("Со свистом разя мечом!");
+        }
+    }
+
+    class Gun : IWeapon
+    {
+        public void AttackWeapon()
+        {
+            Console.WriteLine("Пиу-пиу");
+        }
     }
 }
---
+++ interfaceME/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaceME
+{
+    // Интерфейс IWeapon - описывает поведение оружия.
+
+    // Интерфейс ICharacter - описывает поведение персонажа.У него есть оружие - IWeapon.
+
+    // У нас есть два класса:
+
+    // Warrior(рыцарь) - реализует ICharacter
+    // Sword(меч) - реализует IWeapon
+    // То есть, рыцарь является персонажем и имеет оружие - меч.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface IWeapon
+    {
+        string Weapon { get; set; }
+    }
+    interface ICharacte
+    {
+
+    }
+}
